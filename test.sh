ec3a8a8b978d3bde8ec340903782d8982ec916a7
Ilya <Ilya>
2026-09-11 12:15:10 +0400

    fixing problem with uvicorn

--- test.sh
+++ test.sh
@@ -2,6 +2,8 @@
 
 echo "🧪 Running tests..."
 
+python3 -m venv .venv
+source .venv/bin/activate
 pip install -r requirements.txt
 python3 -m pytest tests/test_unit.py
 python3 -m pytest tests/test_api.py
