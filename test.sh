564d9bb0d6fac3d69b703d0fe1bf7093f4a8f32b
Ilya <Ilya>
2026-09-11 12:52:35 +0400

    fixing problem with tests

--- test.sh
+++ test.sh
@@ -7,5 +7,6 @@ echo "🧪 Running tests..."
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
+nohup uvicorn app.main:app --host 0.0.0.0 --port 8181
 python3 -m pytest tests/test_unit.py
 python3 -m pytest tests/test_api.py
