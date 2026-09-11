5d0eadf1217c894e2817a5a55f8024062a547f90
Ilya <Ilya>
2026-09-11 13:01:11 +0400

    I'm tired, boss

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
 uvicorn app.main:app --host 0.0.0.0 --port 8181 &
-APP_PID = $!
+APP_PID=$!
 trap "kill $APP_PID 2>/dev/null || true" EXIT
 python3 -m pytest tests/test_unit.py
 python3 -m pytest tests/test_api.py
