679d32a9ba8b393f14d02998c7180574855327b1
Ilya <Ilya>
2026-09-11 12:29:00 +0400

    fixing problem with broken pipe

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-source .venv/bin/activate
+python3 -m venv .venv &
+source .venv/bin/activate &
+pip install -r requrements.txt &
 uvicorn app.main:app --reload --host 0.0.0.0 --port 8181
