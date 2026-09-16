80db73fd2d5f6f299d04df26172f7405a8ce58ae
Ilya <Ilya>
2026-09-16 13:24:07 +0400

    fixing problem with autotests

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,30 @@
-#!/usr/bin/env bash
+#!/bin/bash
+echo "🚀 Начинаем развертывание catty-reminders-app..."
 
-echo "🚀 Начинаем развертывание демо-сайта..."
+BRANCH=$1
 
-cp -r ./ /home/catty-reminders-app
-systemctl restart catty-reminders-app
+# Путь к проекту (измените на свой)
+APP_DIR="/home/victor/Desktop/DevOps/Lab_1/catty-reminders-app"
+
+
+# 1. Обновляем код
+echo "📦 Обновляем код из репозитория..."
+cd $APP_DIR
+git fetch origin
+git checkout $BRANCH
+git pull origin $BRANCH
+
+# 2. Если есть зависимости, обновляем их (убедитесь, что pip3 установлен)
+if [ -f "requirements.txt" ]; then
+    source venv/bin/activate
+    echo "📦 Обновляем зависимости..."
+    pip3 install -r requirements.txt
+fi
+
+# 3. Перезапускаем приложение через systemd
+COMMIT_HASH=$(git rev-parse HEAD)
+echo "Код коммита: $COMMIT_HASH"
+echo "DEPLOY_REF=$COMMIT_HASH" > .env
+sudo /usr/bin/systemctl restart catty-app
+
+echo "✅ Развертывание завершено!"
