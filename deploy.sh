c02dd5b1c3276828aae700f1bf355e85e55ec37a
Ilya <Ilya>
2026-09-16 12:15:25 +0400

    testing

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,4 @@
 
 echo "🚀 Начинаем развертывание демо-сайта..."
 
-nohup uvicorn app.main:app --reload --host 0.0.0.0 --port 8181 \
-    > /tmp/uvicorn.log 2>&1 &
+systemctl restart catty-reminders-app
