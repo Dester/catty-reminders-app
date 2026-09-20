3f35b75410ae8ac8d9099df7c66148006b7daee5
Ilya <Ilya>
2026-09-20 06:33:40 +0400

    fixing...

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ ssh $SSH_OPTIONS "$DEPLOY_USER@$DEPLOY_HOST" << EOF
     git checkout $RELEASE_HASH
     
     DEPLOY_REF=\$(git rev-parse HEAD)
-    echo "DEPLOY_REF=\$DEPLOY_REF" > .env.deploy
+    echo "DEPLOY_REF=\$DEPLOY_REF" > /etc/catty-reminders-app.env
     echo "Deployed version: \$DEPLOY_REF"
     
     if [ ! -d ".venv" ]; then
